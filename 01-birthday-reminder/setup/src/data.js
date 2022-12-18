4324edb9e9ceb45591ccf412ff371bf1de3dc509
Ferdaus Zaman Polok <ferdaus.study@gmail.com>
2022-12-18 14:11:07 +0600

    Birthday reminder fianl

--- 01-birthday-reminder/setup/src/data.js
+++ 01-birthday-reminder/setup/src/data.js
@@ -1,37 +1,38 @@
+// eslint-disable-next-line import/no-anonymous-default-export
 export default [
   {
     id: 1,
-    name: 'Bertie Yates',
-    age: 29,
+    name: "Elon Mask",
+    age: 51,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+      "https://upload.wikimedia.org/wikipedia/commons/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg",
   },
   {
     id: 2,
-    name: 'Hester Hogan',
-    age: 32,
+    name: "Bill Gates",
+    age: 67,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+      "https://imageio.forbes.com/specials-images/imageserve/62d599ede3ff49f348f9b9b4/0x0.jpg?format=jpg&crop=821,821,x155,y340,safe&height=416&width=416&fit=bounds",
   },
   {
     id: 3,
-    name: 'Larry Little',
-    age: 36,
+    name: "Mark Zuckerberg",
+    age: 38,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+      "https://static01.nyt.com/images/2020/05/18/business/18tech-newsletter/merlin_153564708_6ecc9ef8-cebb-464e-8729-bd4836438d88-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
   },
   {
     id: 4,
-    name: 'Sean Walsh',
-    age: 34,
+    name: "Steve Jobs",
+    age: 56,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+      "https://cdn.profoto.com/cdn/053149e/contentassets/d39349344d004f9b8963df1551f24bf4/profoto-albert-watson-steve-jobs-pinned-image-original.jpg?width=1280&quality=75&format=jpg",
   },
   {
     id: 5,
-    name: 'Lola Gardner',
-    age: 29,
+    name: "Tim Cook",
+    age: 62,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+      "https://a57.foxnews.com/static.foxbusiness.com/foxbusiness.com/content/uploads/2022/07/931/523/Tim-Cook.jpg?ve=1&tl=1",
   },
 ];
