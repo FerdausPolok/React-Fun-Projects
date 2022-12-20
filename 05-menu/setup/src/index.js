083b260f07507ce05f607791cd97c397ac268bef
Ferdaus Zaman Polok <ferdaus.study@gmail.com>
2022-12-20 16:01:55 +0600

    Menu project final

--- 05-menu/setup/src/index.js
+++ 05-menu/setup/src/index.js
@@ -7,5 +7,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <p className="footerCenterAlign">©️ Ferdaus Zaman Polok</p>
   </React.StrictMode>
 );
